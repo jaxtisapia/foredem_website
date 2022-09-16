0b69e3ebdd7fa4cf720b40e2452e925282d84909
Justice Appiah <32112641+jaxtisapia@users.noreply.github.com>
2022-09-16 09:49:37 +0000

    update about us

--- src/components/modals/AboutUsModal.js
+++ src/components/modals/AboutUsModal.js
@@ -79,6 +79,46 @@ export function AboutUsModal(props) {
               consciousness-keeping our urban and rural areas clean.
             </p>
           </li>
+
+          <li>
+            <p className="margin-bottom-10">
+              Foredem has its objective the contribution to the maintenance of peace which is a necessary ingredient of progress and development. Ethnic conflicts between different tribes have occurred in various parts of the country. Examples of such is Nanumba-konkomba conflicts, kusasi-Mamprugu conflicts, and Dagbamba-konkomba conflicts are just case in point. It's the believe of the organization that these conflicts occured largely as a results of differences in cultural practices or land and other factors.
+            </p>
+            <p className="margin-bottom-10">
+            The institution of chieftaincy play a vital role in the culture of all the various ethnic groups in the country. Chiefs are the custodians of our national heritage, and  leads people in various areas ,and also act as a symbol of our past. These compliment government efforts of good government and some dynamic chiefs leads their people in national development.
+            </p>
+            <p className="margin-bottom-10">
+              However the institution has its share of problems steaming largely from disputes as to who occupies the skin and this has to a large extend undermines its effectiveness.
+            </p>
+            <p className="margin-bottom-10">
+              Examples abound in the country is the unfortunate trend such as the Bawku chieftaincy and tribal disputes involving the Kusasi and the Mamprusi. The Wa chieftaincy which has been resolved and the well known Dagbon case which has pitted Abudu's against the Andani's which has also been resolved and currently the Bimbila skin affairs.
+            </p>
+            <p className="margin-bottom-10">
+              In order for this institution to play a significant role in national development, a lot of measures has to be adobted not only on government level but from private organization. This can take place in a form of reconciliation moves into those areas where such disputes have occured and still in existence.
+              Foredem intends to get involve into such areas and make moves at reconciliation and dialogue. In the case of the Dagbon chieftaincy disputes the organization would still get involve  to find a dialogue between the Abudu's and the Andani's in building resilience for them to abandon their entrench postions and reach a common ground to enable peace and development to take place. Another point that cannot be overlooked is the present crop of chiefs we have especially in the Nothern part of the country.
+            </p>
+            <p className="margin-bottom-10">
+              Largely as a result of lack of education, most of them did not enlightened. If our traditional leaders are not enlightened, they cannot provide the effective leadership that is required of them. This is an area the organization can play an active role organizing workshops in the regional capitals to inform them in wide variety of areas. Foredem can play an active role in terms of reconciling such enthnic groups. This can be captured under the umbrella of reconciliation.
+            </p>
+            <p className="margin-bottom-10">
+              The prolification of arms which has brought these conflicts just mentioned above is an area that deserve the attention of well meaning Ghanaians. This is because it can undermine our democratic practice by promoting ethic conflicts. It is one of the objectives of foredem to educate people about the dangers of the possession of small arms by any persons without any valid reasons. Further more it will lobby with the relevant  groups to bring about the enforcement of laws that will curb the possession of illicit arms. A control of the    possesion of arms could also reduce to a very large extend the incident of armed robbery and counter terrorism in Ghana.
+            </p>
+            <p className="margin-bottom-10">
+              Foredem also intends to work in an area of the provision of potable water to areas that experience acute water stortage especially in the three northern regions which constitute the area of endermic poverty. Largely, as a result over dependence of the people of these regions on the rain water and the water bodies which dry up quickly in the dry seasons, the availability of portable water is seriously undermined. The organization seek to source funding from various areas so as to use the resources at its desposal to engage in borehole drilling to provide an alternate source of potable drinking water to vulnerable communities in these  regions and other areas in the country.
+            </p>
+          </li>
+
+          <li>
+            <p className="margin-bottom-10">
+              Closely connected to the lack of potable water   is the high incidences in guinea worm infestation in many areas in the country. It's the belief of the foundation that with the provision of boreholes guinen worm infested areas will bring about the reduction  and eventual eradication of this life threatening disease. The objective is in line with the belief of foredem that the human resourse capital is the total develoment of the country is very essential. It cannot be over emphasized that guinea worm prevalence has reduced the human resourse potential of many  communities in the country.
+            </p>
+            <p className="margin-bottom-10">
+              With a multi-faceted approach to the solution of many problems in the country, foredem also views the problem of child labour in the country as very serious. In many areas of the country, parental neglect and the abuse of the rights of child is on ascendency. This ranges from refusal of parents to send their children to school and this is endangering their future to using them for economic ventures such as working on farms in the rural areas and selling of wareson the streets in urban cities. It will work closely with relevant agencies that are in the forefront of reducing or bringing an end to child labour.
+            </p>
+            <p className="margin-bottom-10">
+              The children in the country constitutes the future labour force and the leadership in the country. Developing the potential of youth through an emphsis of child education and the establisment of youth training centres to offer and occupation school drop outs is another area of our focus.
+            </p>
+          </li>
         </ul>
       </div>
     </Modal>
